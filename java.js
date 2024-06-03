7a05e6c9552f86fa8f8bccd8e04d4946c2ecc418
wellerson-pinheiros <wellersonpinheiros@outlook.com>
2024-06-02 21:11:38 -0300

    tentando achar uma logica para concluir o desafio
    não achei uma logica para concluir o desafio tentei a estrutura de repetiçao for e o while mais sem sucesso

---
+++ java.js
@@ -0,0 +1,18 @@
+function calcular(){
+let nub1 = 2
+let nub2 = 2
+let guarda = window.prompt(`qual o resultado de ${nub1} + ${nub2}`)
+
+            
+       
+        while(guarda != 4){
+     window.prompt('burro')
+break;}
+
+
+   while (guarda + guarda ){
+    window.prompt(`qual o resultado de ${guarda} + ${guarda}`)
+   }
+}
+
+
