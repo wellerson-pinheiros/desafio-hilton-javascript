1cf3b7d403ccef2e8f93d8ace76d22b6f0c66383
wellerson-pinheiros <wellersonpinheiros@outlook.com>
2024-06-03 23:53:18 -0300

    alteraçao no codigo
    tentando fazer um laço de repetiçao com soma

---
+++ javascript.js
@@ -0,0 +1,19 @@
+function calcular(){
+let nub1 = 2
+let nub2 = 2
+let guarda = window.prompt(`qual o resultado de ${nub1} + ${nub2}`)
+
+               if(guarda == 4 ){
+                
+                guarda = soamr()
+                 
+               }else {window.alert('burro')
+
+               }
+}  
+
+function somar(){
+      while(true){
+      let soma = guarda += guarda
+      window.prompt(`quanto é ${guarda}+${guarda}` )
+}}
